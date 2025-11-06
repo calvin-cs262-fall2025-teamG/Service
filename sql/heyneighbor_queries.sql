8a0fdf1ca269614c3788fbf789b117a7da984f82
maham-creates <mahamtulips@gmail.com>
2025-11-06 14:57:15 -0500

    Update heyneighbor_queries.sql

--- sql/heyneighbor_queries.sql
+++ sql/heyneighbor_queries.sql
@@ -16,6 +16,30 @@ FROM Item i
 JOIN "User" u ON i.owner_id = u.user_id
 WHERE i.status = 'available'
 
+-- show active borrowing requests
+SELECT br.request_id, u1.name AS borrower, u2.name AS lender, i.name AS item, br.status, br.request_datetime
+FROM BorrowingRequest br
+JOIN "User" u1 ON br.borrower_id = u1.user_id
+JOIN "User" u2 ON br.lister_id = u2.user_id
+JOIN Item i ON br.item_id = i.item_id
+WHERE br.status = 'pending';
+
+-- show borrowing history for a specific user (borrower_id = 2)
+SELECT u.name AS borrower, i.name AS item, bh.returned, bh.return_date
+FROM BorrowingHistory bh
+JOIN "User" u ON bh.borrower_id = u.user_id
+JOIN Item i ON bh.item_id = i.item_id
+WHERE bh.borrower_id = 2;
+
+-- list lending history
+SELECT u.name AS lender, i.name AS item, lh.availability_duration
+FROM LendingHistory lh
+JOIN "User" u ON lh.lender_id = u.user_id
+JOIN Item i ON lh.item_id = i.item_id;
+
+
+
+
 
 
 
